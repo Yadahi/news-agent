12a1003898bb5c53fc9ce43839d807eb858facbc
Viliam Vateha <vatvil@gmail.com>
2026-04-29 07:48:47 -0300

    add auth controller

---
+++ api/controllers/authController.js
@@ -0,0 +1,58 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const db = require("../db");
+const AppError = require("../utils/AppError");
+
+function register(req, res, next) {
+  try {
+    const { username, password } = req.body;
+
+    const hashedPassword = bcrypt.hashSync(password, 10);
+
+    const stmt = db
+      .prepare("INSERT INTO users (username, password) VALUES (?, ?)")
+      .run(username, hashedPassword);
+
+    res.status(201).json({ message: "User registered" });
+  } catch (error) {
+    if (error.message.includes("UNIQUE constraint failed")) {
+      next(new AppError("User exist", 409));
+      return;
+    }
+    next(error);
+  }
+}
+
+function login(req, res, next) {
+  try {
+    const { username, password } = req.body;
+
+    const stmt = db.prepare("SELECT * FROM users WHERE username=?");
+    const result = stmt.get(username);
+
+    const hashedPassword = result?.password;
+
+    if (result) {
+      const passwordMatch = bcrypt.compareSync(password, hashedPassword);
+      if (!passwordMatch) {
+        throw new AppError("Invalid credentials", 401);
+      }
+
+      const token = jwt.sign(
+        { id: result.id, username: result.username },
+        process.env.SECRET_KEY,
+        {
+          expiresIn: "1h",
+        },
+      );
+
+      res.status(200).json({ message: "User logged in", token });
+    } else {
+      throw new AppError("Invalid credentials", 401);
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = { register, login };
